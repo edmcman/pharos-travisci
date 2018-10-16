da93a26550438da1e442faf2c994cf8fc34a3a2e
Edward J. Schwartz <eschwartz@cert.org>
2018-10-16 11:38:52 -0400

    Try this again.

--- scripts/build.bash
+++ scripts/build.bash
@@ -12,7 +12,7 @@ cd $DIR
 
 wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
 test -d cmake-3.8.2 && rm -rf cmake-3.8.2
-tar -xvf cmake-3.8.2.tar.gz
+tar -xzf cmake-3.8.2.tar.gz
 cd cmake-3.8.2
 ./bootstrap
 make -j 4
