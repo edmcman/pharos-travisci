576590efc644c792db6b634c8f198f92b1ca82d4
Edward J. Schwartz <eschwartz@cert.org>
2018-10-16 20:46:24 -0400

    Why is this failing?

--- scripts/build.bash
+++ scripts/build.bash
@@ -71,7 +71,7 @@ cd release
   --enable-languages=binaries --enable-projects-directory \
   --disable-tutorial-directory --disable-boost-version-check \
   --with-boost=/usr/local CXXFLAGS=-std=c++11 --with-yaml=/usr/local \
-  --with-z3=/usr/local
+  --with-z3=/usr/local || cat config.log | tail -n 100
 make -j4
 sudo make -j4 install
 
